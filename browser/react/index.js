584a20b05665984a9e2481f15f6e0b003fd947f5
Jacob Regan <jacobmregan0@gmail.com>
2017-05-18 09:22:13 -0500

    Initialized project

---
+++ browser/react/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+
+import AppContainer from './containers/AppContainer';
+import Albums from './components/Albums';
+import Album from './components/Album';
+import Artists from './components/Artists';
+import Artist from './components/Artist';
+import Songs from './components/Songs';
+
+const router = (
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer}>
+      <IndexRedirect to="/albums" />
+      <Route path="/albums" component={Albums} />
+      <Route path="albums/:albumId" component={Album} />
+
+      <Route path="/artists" component={Artists} />
+      <Route path="/artists/:artistId" component={Artist}>
+        <Route path="albums" component={Albums} />
+        <Route path="songs" component={Songs} />
+      </Route>
+    </Route>
+  </Router>
+);
+
+ReactDOM.render(
+  router,
+  document.getElementById('app'),
+);
